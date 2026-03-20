825d37870ec7f27771818e56cae102d6ccf4367e
AlbertoRosado1 <ar652820@ohio.edu>
2026-03-20 09:07:40 -0700

    added paths to Holi v3 mocks

--- clustering_statistics/tools.py
+++ clustering_statistics/tools.py
@@ -836,6 +836,21 @@ def get_catalog_fn(version=None, cat_dir=None, kind='data', tracer='LRG',
             ext = 'fits' if 'full' in kind else 'h5'
             if kind == 'forfa_data':
                 return base_dir / f'forFA{imock:d}.fits'
+        
+        elif version == 'holi-v3-complete':
+            cat_dir = desi_dir / f'mocks/cai/LSS/DA2/mocks/holi_v3/altmtl{imock:d}/loa-v1/mock{imock:d}/LSScats'
+            ext = 'h5' if 'full' in kind else 'h5'
+            if kind == 'data':
+                return cat_dir / f'{tracer}_complete_{region}_clustering.dat.{ext}'
+            if kind == 'randoms':
+                return [cat_dir / f'{tracer}_complete_{region}_{iran:d}_clustering.ran.{ext}' for iran in range(nran)]
+        
+        elif version == 'holi-v3-altmtl':
+            base_dir = desi_dir / f'mocks/cai/LSS/DA2/mocks/holi_v3'
+            cat_dir = base_dir / f'altmtl{imock:d}/loa-v1/mock{imock:d}/LSScats'
+            ext = 'fits' if 'full' in kind else 'h5'
+            if kind == 'forfa_data':
+                return base_dir / f'forFA{imock:d}.fits'
 
         elif version == 'glam-uchuu-v1-altmtl':
             base_dir = desi_dir / f'mocks/cai/LSS/DA2/mocks/GLAM-Uchuu_v1'
