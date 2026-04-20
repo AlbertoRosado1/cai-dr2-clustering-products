Fix data splits covariance and HOLI mock rerun checks

clustering_statistics/job_scripts/desipipe_holi_mocks.py
@@ -5,7 +5,6 @@ To create and spawn the tasks on NERSC, use the following commands:
 salloc -N 1 -C "gpu&hbm80g" -t 04:00:00 --gpus 4 --qos interactive --account desi_g
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 export PYTHONPATH=$HOME/cai-dr2-clustering-products/:$PYTHONPATH
-srun -n 4 python desipipe_holi_mocks.py # if mode in interative
 python desipipe_holi_mocks.py         # create the list of tasks
 desipipe tasks -q holi_mocks          # check the list of tasks
 desipipe spawn -q holi_mocks --spawn  # spawn the jobs
@@ -36,6 +35,13 @@ tm80 = tm.clone(provider=dict(provider='nersc', time='02:00:00',
                             mpiprocs_per_worker=4, output=output, error=error, stop_after=1, constraint='gpu&hbm80g'))
 tmw = tm.clone(scheduler=dict(max_workers=1), provider=dict(provider='nersc', time='00:10:00',
                 mpiprocs_per_worker=2250, nodes_per_worker=25, output=output, error=error, stop_after=1, constraint='cpu'))
+
+combine_region_sources = {
+    'GCcomb': ['NGC', 'SGC'],
+    'NS': ['N', 'S'],
+    'GCcomb_noN': ['NGCnoN', 'SGC'],
+    'GCcomb_noDES': ['NGC', 'SGCnoDES'],
+}
 
 def run_stats(tracer='LRG', project='', version='holi-v3-altmtl', onthefly=None, imocks=[150], stats_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum'], weight='default-FKP', analysis='full_shape', regions=['NGC','SGC'], ibatch=None, postprocess=None, zranges=None, **kwargs):
     # Everything inside this function will be executed on the compute nodes;
@@ -85,21 +91,24 @@ def run_stats(tracer='LRG', project='', version='holi-v3-altmtl', onthefly=None,
                                    combine_regions={'stats': stats}, mesh2_spectrum=mesh2_spectrum, window_mesh2_spectrum=window_mesh2_spectrum)
         postprocess_stats_from_options(postprocess, analysis=analysis, get_stats_fn=get_stats_fn, **postprocess_options)
 
-
-def postprocess_stats(tracer='LRG', analysis='full_shape', project='', version='holi-v3-altmtl', onthefly=None, imocks=[150], stats_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum'], weight='default-FKP', postprocess=['combine_regions'], zranges=None, **kwargs):
+def postprocess_stats(tracer='LRG', analysis='full_shape', project='', version='holi-v3-altmtl', onthefly=None, imocks=[150], stats_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum'], weight='default-FKP', postprocess=['combine_regions'], zranges=None, regions = ['GCcomb'], **kwargs):
     from clustering_statistics import postprocess_stats_from_options
+    if len(imocks) == 0:
+        return
     if zranges is None:
         zranges = tools.propose_fiducial('zranges', tracer, analysis=analysis)
-    options = dict(catalog=dict(version=version, tracer=tracer, zrange=zranges, weight=weight, imock=imocks[0]), imocks=imocks, combine_regions={'stats': stats}, mesh2_spectrum={'cut': True, 'auw': True}, window_mesh2_spectrum={'cut': True})
-    stats_dir_kws = dict(stats_dir=stats_dir, project=project)
-    if onthefly == 'complete':
-        get_stats_fn = functools.partial(tools.get_stats_fn, extra='complete', **stats_dir_kws)
-    elif onthefly == 'reshuffle':
-        get_stats_fn = functools.partial(tools.get_stats_fn, extra='reshuffle', **stats_dir_kws)
-    else:
-        get_stats_fn = functools.partial(tools.get_stats_fn, **stats_dir_kws)
-
-    postprocess_stats_from_options(postprocess, analysis=analysis, get_stats_fn=get_stats_fn, **options)
+    for region in regions:
+        options = dict(catalog=dict(version=version, tracer=tracer, zrange=zranges, region=region, weight=weight, imock=imocks[0]), imocks=imocks, combine_regions={'stats': stats}, 
+                    mesh2_spectrum={'cut': True, 'auw': True}, window_mesh2_spectrum={'cut': True})
+        options.update(combine_regions={'stats': stats, 'regions': combine_region_sources.get(region, ['NGC', 'SGC'])})
+        stats_dir_kws = dict(stats_dir=stats_dir, project=project)
+        if onthefly == 'complete':
+            get_stats_fn = functools.partial(tools.get_stats_fn, extra='complete', **stats_dir_kws)
+        elif onthefly == 'reshuffle':
+            get_stats_fn = functools.partial(tools.get_stats_fn, extra='reshuffle', **stats_dir_kws)
+        else:
+            get_stats_fn = functools.partial(tools.get_stats_fn, **stats_dir_kws)
+        postprocess_stats_from_options(postprocess, analysis=analysis, get_stats_fn=get_stats_fn, **options)
 
 
 if __name__ == '__main__':
@@ -117,7 +126,7 @@ if __name__ == '__main__':
     # to run job
     # mode = 'slurm'
     mode = 'interactive'
-    imocks2run = np.arange(100,200)
+    imocks2run = np.arange(0,199)
     if version == 'holi-v3-altmtl':
         # do not perform measurements on dubious mocks
         bad_imocks = np.loadtxt('../helper_scripts/dubious_holi-v3-altmtl.txt',dtype=int)
@@ -131,6 +140,7 @@ if __name__ == '__main__':
     # project  = f'{analysis}/base'
     # weight   = 'default-FKP'
     # regions  = ['NGC','SGC']
+    # tracers  = ['LRG', 'ELG_LOPnotqso', 'QSO']
     # max_mocks_per_batch = 10
 
     # run data_splits for lensing group with full_shape setup 
@@ -166,18 +176,21 @@ if __name__ == '__main__':
         if check_for_existing_measurements:
             exists, missing = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_catalog_fn, tracer=tracer[0] if isinstance(tracer, (list, tuple)) else tracer,
                                                                                            region='NGC', version=version), test_if_readable=False, imock=imocks2run)[:2]
-            imocks = exists[1]['imock']
-            rerun = []
+            catalog_imocks = exists[1]['imock']
+            rerun_by_region = {region: [] for region in regions}
             for zrange in zranges:
                 for kind in stats:
-                    stats_kws = dict(basis='sugiyama-diagonal', kind=kind, stats_dir=Path(str(stats_dir).replace('global','dvs_ro')), 
-                                     tracer=tracer, region=regions[-1], weight=weight, zrange=zrange, version=version, project=project,
-                                     extra=onthefly if onthefly else '')
-                    rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_stats_fn, **stats_kws), test_if_readable=True, imock=imocks2run)
-                    rerun += [imock for imock in imocks if (imock in unreadable[1]['imock']) or (imock not in rexists[1]['imock'])]
-            imocks = sorted(set(rerun))
+                    for region in regions:
+                        stats_kws = dict(basis='sugiyama-diagonal', kind=kind, stats_dir=Path(str(stats_dir).replace('global','dvs_ro')),
+                                         tracer=tracer, region=region, weight=weight, zrange=zrange, version=version, project=project,
+                                         extra=onthefly if onthefly else '')
+                        rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_stats_fn, **stats_kws), test_if_readable=True, imock=imocks2run)
+                        rerun_by_region[region] += [imock for imock in catalog_imocks if (imock in unreadable[1]['imock']) or (imock not in rexists[1]['imock'])]
+            rerun_by_region = {region: sorted(set(rerun)) for region, rerun in rerun_by_region.items()}
+            imocks = sorted(set(imock for rerun in rerun_by_region.values() for imock in rerun))
         else:
             imocks = imocks2run
+            rerun_by_region = {region: imocks for region in regions}
        
         def get_run_stats():
             _tm = tm80
@@ -202,8 +215,23 @@ if __name__ == '__main__':
             elif any('covariance' in stat for stat in stats):
                 get_run_stats()(imocks=[201], **run_stats_kws)
             elif stats:
-                batch_imocks = np.array_split(imocks, max(len(imocks) // max_mocks_per_batch, 1)) if len(imocks) else []
-                for _imocks in batch_imocks:
-                    get_run_stats()(imocks=_imocks, **run_stats_kws)
-        # if postprocess:
-        #     postprocess_stats(imocks=imocks, **run_stats_kws)
+                for region, region_imocks in rerun_by_region.items():
+                    batch_imocks = np.array_split(region_imocks, max(len(region_imocks) // max_mocks_per_batch, 1)) if len(region_imocks) else []
+                    for _imocks in batch_imocks:
+                        get_run_stats()(imocks=_imocks, **(run_stats_kws | dict(regions=[region])))
+
+        if postprocess:
+            if check_for_existing_measurements:
+                postprocess_rerun = []
+                for zrange in zranges:
+                    for kind in stats:
+                        for region in postregions:
+                            stats_kws = dict(basis='sugiyama-diagonal', kind=kind, stats_dir=Path(str(stats_dir).replace('global','dvs_ro')),
+                                             tracer=tracer, region=region, weight=weight, zrange=zrange, version=version, project=project,
+                                             extra=onthefly if onthefly else '')
+                            rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_stats_fn, **stats_kws), test_if_readable=True, imock=imocks2run)
+                            postprocess_rerun += [imock for imock in imocks2run if (imock in unreadable[1]['imock']) or (imock not in rexists[1]['imock'])]
+                imocks = sorted(set(postprocess_rerun))
+            else:
+                imocks = imocks2run
+            postprocess_stats(imocks=imocks, **(run_stats_kws | dict(regions=postregions)))